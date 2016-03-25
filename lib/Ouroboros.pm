86620d0bac3c4b599b11db8ad571119ebf37937a
Vickenty Fesunov <kent@setattr.net>
2016-03-26 00:40:02 +0100

    Release v0.02-TRIAL.

--- lib/Ouroboros.pm
+++ lib/Ouroboros.pm
@@ -67,7 +67,7 @@ our %EXPORT_TAGS = (
 
 our @EXPORT = qw();
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 require XSLoader;
 XSLoader::load('Ouroboros', $VERSION);
--- lib/Ouroboros/Spec.pm
+++ lib/Ouroboros/Spec.pm
@@ -2,7 +2,7 @@ package Ouroboros::Spec;
 use strict;
 use warnings;
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 # spec {
 our %SPEC = (
